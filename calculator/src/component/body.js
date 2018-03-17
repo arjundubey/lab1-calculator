ef4758f6c6bf9bc0dcd8af409d1e9f9ce67bc1e7
arjun <arjun.dubey@sjsu.edu>
2018-03-17 08:00:13 -0700

    functiolaity compiles

--- calculator/src/component/body.js
+++ calculator/src/component/body.js
@@ -2,15 +2,66 @@ import React from 'react';
 import Button from './button';
 
 class calcBody extends React.Component {
+         
+        
+
+
+    inputPercent(){
+          const{displayValue}=this.state
+          const value=parseFloat(displayValue)
+          this.setState({
+            displayValue:String(value/100)
+          })
+        }
+
+
+
 	 render () {
+       const{displayValue}=this.state
+
+
+
+
 	 	      return(
                     <div className="calculator-body clearfix">
           <Button label="+" />
           <Button label="-" />
-          <Button label="/" />
+          <Button className="calculator-key" onClick={()=>this.performOperation('/')} label="/" />
           <Button label="*" />
-          
-      </div>
+
+
+            
+
+
+
+                
+
+
+      </ div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	 	      	)
 	 }
--- calculator/src/component/button.js
+++ calculator/src/component/button.js
@@ -10,12 +10,20 @@ class calcButton extends React.Component {
 			 	      return(
 		          <button onClick ={this.clicked} className="btn btn-primary">{this.props.label}</button>
 
+
+                      <div className="operator-key">
+                      <button className="calculator key  key-add">+</ button>
+                      <button className="calculator key  key-subtract">-</ button>
+                      <button className="calculator key  key-divide">/</ button>
+                      <button className="calculator key  key-multiply">*</ button>
+                      </ div>
+
 	 	      	)
 	 }
 
   
 }
-
+ 
 
  
 
